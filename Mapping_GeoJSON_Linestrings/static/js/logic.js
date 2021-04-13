51bf8f92f30de49915b7d360c964b683971f94ac
James Benasuli <jamiebenasuli@gmail.com>
2021-04-12 21:19:11 -0400

    update linestrings formatting

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -12,7 +12,7 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
     accessToken: API_KEY
 });
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       accessToken: API_KEY
@@ -33,26 +33,25 @@ let map = L.map('mapid', {
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
 let torontoData = 'https://raw.githubusercontent.com/jbenasuli/Mapping_Earthquakes/main/torontoRoutes.json';
 
-L.control.layers(baseMaps).addTo(map);
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup('<h3> Airline: ' + feature.properties.airline + '</h3><hr><h3> Destination: ' 
+    + feature.properties.dst + '</h3>');
+  }
+})
+.addTo(map);
 });
-
-// // Grabbing our GeoJSON data.
-// d3.json(airportData).then(function(data) {
-//     console.log(data);
-//   // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJson(data, {
-//     onEachFeature: function(feature, layer) {
-//         console.log(layer);
-//         layer.bindPopup("<h2>" + "airport code: " + feature.properties.faa + "<br>" + "<hr>" + "airport name: " + feature.properties.name + "</h2>")
-//     }
-//   }).addTo(map);
-// });
-
