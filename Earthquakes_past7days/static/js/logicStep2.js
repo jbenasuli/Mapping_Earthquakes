929317fc82755dd5c1cea775959e843fc863fdfa
James Benasuli <jamiebenasuli@gmail.com>
2021-04-13 01:31:56 -0400

    add step 2

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -14,12 +14,6 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     accessToken: API_KEY
 });
 
-// let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//       attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//       maxZoom: 18,
-//       accessToken: API_KEY
-//   });
-
 // Create a base layer that holds both maps.
 let baseMaps = {
     'Streets': streets,
@@ -33,31 +27,39 @@ let map = L.map('mapid', {
     layers: [streets]
 })
 
-// // Pass our map layers into our layers control and add the layers control to the map.
-// L.control.layers(baseMaps).addTo(map);
-
-// // Accessing the Toronto neighborhoods GeoJSON URL.
-// let torontoHoods = 'https://raw.githubusercontent.com/jbenasuli/Mapping_Earthquakes/main/torontoNeighborhoods.json';
-
-// // Create a style for the lines.
-// let myStyle = {
-//   color: '#1100ff',
-//   weight: 1,
-//   fillColor: '#ffff00',
-// }
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Grabbing our GeoJSON data.
 d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson').then(function(data) {
   console.log(data);
+  // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+}
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo
+  }).addTo(map);
 });
   
-//   L.geoJson(data, {
-//       style: myStyle,
-//       onEachFeature: function(feature, layer) {
-//         layer.bindPopup('<h3> Neighborhood: ' + feature.properties.AREA_NAME + '</h3>');
-//       }
-//     })
-//     .addTo(map)
-// });
